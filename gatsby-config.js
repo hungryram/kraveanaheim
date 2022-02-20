de9d7ff7e7a829b164bc7a9845f0c73873dbce17
Ram Dettmer <ramdettmer751@gmail.com>
2022-02-19 22:19:26 -0800

    update analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,10 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // The property ID; the tracking code won't be generated without it
         trackingId: "G-SM1VXDM7ZS",
-        // Defines where to place the tracking script - `true` in the head and `false` in the body
-        head: true,
       },
     },
   ],
