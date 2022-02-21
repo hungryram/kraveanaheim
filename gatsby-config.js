bd8d94a97f054fc1dc8fdc30c8e78f1f581d9cc5
Ram Dettmer <ramdettmer751@gmail.com>
2022-02-21 13:59:18 -0800

    add analytics gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,12 +30,6 @@ module.exports = {
       },
     },
     {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: "G-SM1VXDM7ZS",
-      },
-    },
-    {
       resolve: `gatsby-plugin-google-gtag`,
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
